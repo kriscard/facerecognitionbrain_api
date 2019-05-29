5e95c55f87abc879eb015a1e714ea0db88638dbe
Christopher Cardoso <cardoso.christopher01@gmail.com>
2019-05-29 14:25:28 +0200

    updating database link

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const profile = require("./controllers/profile");
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
+    host: "postgresql-silhouetted-79015",
     user: "christopher",
     password: "",
     database: "smart-brain"
