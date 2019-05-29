ce0dcf736464fde4b85c63b4e47cf2cb896ca8a6
Christopher Cardoso <cardoso.christopher01@gmail.com>
2019-05-29 17:11:53 +0200

    hide api key call

--- controllers/image.js
+++ controllers/image.js
@@ -1,7 +1,7 @@
 const Clarifai = require("clarifai");
 
 const app = new Clarifai.App({
-  apiKey: "f4920ea5eef440e29424cb479cb19ac6"
+  apiKey: process.env.API_CLARIFAI
 });
 
 const handleApiCall = (req, res) => {
